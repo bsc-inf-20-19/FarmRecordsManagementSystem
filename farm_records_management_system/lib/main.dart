a8b26ca64213df25ceb1681e35f1276e6e512d28
Harold-Mvula <bsc-inf-01-19@unima.ac.mw>
2024-04-01 08:26:32 +0200

    This is home page of the application

--- farm_records_management_system/lib/main.dart
+++ farm_records_management_system/lib/main.dart
@@ -1,125 +1,154 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void navigateToDetailsPage(BuildContext context, String cardTitle) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DetailsPage(cardTitle: cardTitle),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        drawer: Container(alignment: Alignment.topLeft, color: Colors.white, height: 250,),
+        backgroundColor: const Color.fromARGB(
+            255, 169, 175, 175), // Change background color here
+        appBar: AppBar(
+          backgroundColor: Colors.blue, // Change background color here
+          title: const Text('Farm Records Management'),
+          titleTextStyle:
+              const TextStyle(fontStyle: FontStyle.normal, fontSize: 20),
+          centerTitle: true,
+        ),
+        body:  Center(
+          child: Column(
+           mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    CardItem(
+                        title: 'Crops & Fields',
+                        onTap: () {
+                         Navigator.push(context, MaterialPageRoute(
+                           builder: (context) =>const DetailsPage(cardTitle: 'First Card'),
+                  ),
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+                    CardItem(
+                        title: 'Reports',
+                        onTap: () {
+                          navigateToDetailsPage(context, 'reports');
+                        })
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    CardItem(
+                      title: 'Transactions',
+                      onTap: () {
+                        navigateToDetailsPage(context, 'Card 1');
+                      },
+                    ),
+                    CardItem(
+                      title: 'Crops Comparison',
+                      onTap: () {
+                        navigateToDetailsPage(context, 'Card 2');
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    CardItem(
+                      title: 'Card 5',
+                      onTap: () {
+                        navigateToDetailsPage(context, 'Card 2');
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+
+class CardItem extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+
+  const CardItem({super.key, required this.title, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(16.0),
+      child: InkWell(
+        onTap: onTap,
+        // () {
+        //   // Add your onTap logic here
+        // },
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          height: 150,
+          width: 150,
+          alignment: Alignment.bottomRight,
+          child: Center(
+            child: Text(
+              title,
+              style: const TextStyle(fontSize: 20.0),
             ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class DetailsPage extends StatelessWidget {
+  final String cardTitle;
+
+  const DetailsPage({super.key, required this.cardTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Details for $cardTitle'),
+      ),
+      body: Center(
+        child: Text(
+          'Details for $cardTitle will be shown here.',
+          style:const TextStyle(fontSize: 24),
+        ),
+      ),
     );
   }
 }
