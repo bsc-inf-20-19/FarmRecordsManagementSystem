6290b0854979477583d6237320acc6ea1bf42ed1
Harold-Mvula <bsc-inf-01-19@unima.ac.mw>
2024-06-13 03:13:13 +0200

    deleted files

--- farm_records_management_system/lib/screens/crops_livestock.dart
+++ farm_records_management_system/lib/screens/crops_livestock.dart
@@ -1,63 +1,63 @@
-import 'package:farm_records_management_system/Pages/home.dart';
-import 'package:farm_records_management_system/screens/new_planting.dart';
-import 'package:farm_records_management_system/widgets/crops_page.dart';
-import 'package:flutter/material.dart';
+// import 'package:farm_records_management_system/Pages/home.dart';
+// import 'package:farm_records_management_system/screens/new_planting.dart';
+// import 'package:farm_records_management_system/widgets/crops_page.dart';
+// import 'package:flutter/material.dart';
 
-class CropsLivestockPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 16, 83, 8),
-        body: LandingPage(),
-      ),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
+// class CropsLivestockPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Color.fromARGB(255, 16, 83, 8),
+//         body: LandingPage(),
+//       ),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
 
-class LandingPage extends StatelessWidget {
-  const LandingPage({Key? key});
+// class LandingPage extends StatelessWidget {
+//   const LandingPage({Key? key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text('Crops and Livestock'),
-        titleTextStyle: const TextStyle(fontStyle: FontStyle.normal, fontSize: 20),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                CardItem(
-                  title: 'Crops',
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MyActivityPage()),
-                    );
-                  },
-                ),
-                CardItem(
-                  title: 'Livestock',
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => NewPlantPage()),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.blue,
+//         title: const Text('Crops and Livestock'),
+//         titleTextStyle: const TextStyle(fontStyle: FontStyle.normal, fontSize: 20),
+//         centerTitle: true,
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [
+//                 CardItem(
+//                   title: 'Crops',
+//                   onTap: () {
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(builder: (context) => MyActivityPage()),
+//                     );
+//                   },
+//                 ),
+//                 CardItem(
+//                   title: 'Livestock',
+//                   onTap: () {
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(builder: (context) => NewPlantPage()),
+//                     );
+//                   },
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
